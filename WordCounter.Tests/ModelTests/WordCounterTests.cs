c22501f888acb76c1b2db54123b27316fed60c48
JakeAsh22 <jacob.ash1998@gmail.com>
2019-12-20 10:45:06 -0800

    First test will fail correctly

---
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using WordCounting;
+
+namespace WordCountingTests
+{
+    [TestClass]
+    public class WordCounterTest
+    {
+        [TestMethod]
+        public void WordCounting_GetResult_NotAWord()
+        {
+            // Counter count1 = new Counter ("1", "1111111");
+            // string result = count1.RepeatCounter();
+            // Assert.AreEqual("Please only use letters", result);
+
+        }
+    }
+}
---
+++ WordCounter/Models/WordCounter.cs
@@ -0,0 +1,30 @@
+using System;
+
+// namespace WordCounting
+// {
+//     public class WordCounter
+//     {
+//         private string _word;
+//         private string _sentence;
+//         private int _wordCount;
+//         string numbers = "0123456789";
+
+//         public WordCounter(string word, string sentence)
+//         {
+//             _word = word;
+//             _sentence = sentence;
+//         }
+
+//         public string RepeatCounter()
+//         {
+//             // string lowerWord = _word.ToLower();
+//             // string lowerSentence = _sentence.ToLower();
+//             // for (int i =0; i<lowerSentence.Length;i++)
+//             // {
+//             //     if (numbers.contains(lowerSentence[i]))
+//             //         return "Please only use letters";
+//             // }
+//             // return "Placeholder";
+//         }
+//     }
+// }
